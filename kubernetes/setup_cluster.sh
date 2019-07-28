40358c4ab52b755844d13886818052f89a59c6ea
Sergey Kushnerchuk <s.kushnerchuk@frontol.ru>
2019-07-28 11:30:53 +0300

    Revert "Доработка скриптов развертывания и описания проекта"
    This reverts commit 92a0e2f0fe2408675e4698b62fde5007c02ab659.

--- kubernetes/setup_cluster.sh
+++ kubernetes/setup_cluster.sh
@@ -1,8 +1,6 @@
-helm upgrade prometheus1 charts/prometheus/ --install --namespace review
-helm upgrade grafana11 charts/grafana/ --install --namespace review
+helm upgrade prometheus charts/prometheus/ --install
+helm upgrade grafana charts/grafana/ --install
 helm dep update charts/efk
-helm upgrade efk1 charts/efk/ --install --namespace review
-helm dep update charts/rabbitmongo
-helm upgrade rm1 charts/rabbitmongo  --install --namespace review
-helm upgrade ui1 charts/ui --install --set mongodbHost=rm-mongodb --namespace review
-helm upgrade bot1 charts/bot --install --set mongodbHost=rm-mongodb --set rabbitmqHost=rm-rabbitmq --namespace review
+helm upgrade efk charts/efk/ --install
+helm dep update charts/crawler
+helm upgrade crawler charts/crawler/ --install
