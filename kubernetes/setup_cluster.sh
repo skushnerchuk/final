92a0e2f0fe2408675e4698b62fde5007c02ab659
Sergey Kushnerchuk <s.kushnerchuk@frontol.ru>
2019-07-28 11:30:53 +0300

    Доработка скриптов развертывания и описания проекта

--- kubernetes/setup_cluster.sh
+++ kubernetes/setup_cluster.sh
@@ -1,6 +1,8 @@
-helm upgrade prometheus charts/prometheus/ --install
-helm upgrade grafana charts/grafana/ --install
+helm upgrade prometheus1 charts/prometheus/ --install --namespace review
+helm upgrade grafana11 charts/grafana/ --install --namespace review
 helm dep update charts/efk
-helm upgrade efk charts/efk/ --install
-helm dep update charts/crawler
-helm upgrade crawler charts/crawler/ --install
+helm upgrade efk1 charts/efk/ --install --namespace review
+helm dep update charts/rabbitmongo
+helm upgrade rm1 charts/rabbitmongo  --install --namespace review
+helm upgrade ui1 charts/ui --install --set mongodbHost=rm-mongodb --namespace review
+helm upgrade bot1 charts/bot --install --set mongodbHost=rm-mongodb --set rabbitmqHost=rm-rabbitmq --namespace review
