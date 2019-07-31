36039602ef7f08ca36a0d3795da11c1dca8c6dc0
Sergey Kushnerchuk <s.kushnerchuk@frontol.ru>
2019-07-31 21:50:23 +0300

    Исправление скриптов Уточнение документации

--- deploy_scripts/setup_cluster.sh
+++
@@ -1,8 +0,0 @@
-helm upgrade prometheus charts/prometheus/ --install
-helm upgrade grafana charts/grafana/ --install
-helm dep update charts/efk
-helm upgrade efk charts/efk/ --install
-helm dep update charts/rabbitmongo
-helm upgrade rm charts/rabbitmongo --install
-helm upgrade ui charts/ui --install --set mongodbHost=rm-mongodb
-helm upgrade bot charts/bot --install --set mongodbHost=rm-mongodb --set rabbitmqHost=rm-rabbitmq
